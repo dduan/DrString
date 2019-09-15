8fb44f46fdb38a357998864e86fc7e7a4a6ed209
Daniel Duan <daniel@duan.ca>
2019-09-15 12:41:08 -0700

    Add terminal colors
    Yay

--- Sources/DrInformant/TtyTextFormatter.swift
+++ Sources/DrInformant/TtyTextFormatter.swift
@@ -1,16 +1,17 @@
+import Chalk
 import DrCritic
 
 private extension DocProblem.Detail {
     var description: String {
         switch self {
         case .redundantParameter(let name):
-            return "Unrecognized docstring for `\(name)`"
+            return "Unrecognized docstring for \(name, color: .green)"
         case .missingParameter(let name, let type):
-            return "Missing docstring for `\(name)` of type `\(type)`"
+            return "Missing docstring for \(name, color: .green) of type \(type, color: .cyan)"
         case .missingThrow:
             return "Missing docstring for throws"
         case .missingReturn(let type):
-            return "Missing docstring for return type `\(type)`"
+            return "Missing docstring for return type \(type, color: .cyan)"
         }
     }
 }
@@ -19,12 +20,13 @@ private extension DocProblem {
     var description: String {
         let count = self.details.count
         let pluralPostfix = count > 1 ? "s" : ""
-        let header = "\(self.filePath):\(self.line):\(self.column): warning: \(count) docstring problem\(pluralPostfix) regarding `\(self.docName)`"
+        let headerText = "\(self.filePath):\(self.line):\(self.column): \("warning", color: .yellow): \(count) docstring problem\(pluralPostfix) regarding \(self.docName, color: .green)"
+        let header = "\(headerText, style: .bold)"
         let detailPrefix = " - "
         return ([header] + self.details.map { detailPrefix + $0.description }).joined(separator: "\n")
     }
 }
 
-public func plainText(for docProblem: DocProblem) -> String {
+public func ttyText(for docProblem: DocProblem) -> String {
     return docProblem.description
 }
