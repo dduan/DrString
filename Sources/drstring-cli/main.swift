2eeb89193087c8c61f84753d1c7513c7a5b0af3d
Daniel Duan <daniel@duan.ca>
2019-09-15 22:31:27 -0700

    Support TOML config file

--- Sources/drstring-cli/main.swift
+++ Sources/drstring-cli/main.swift
@@ -1,5 +1,7 @@
+import Foundation
 import DrString
 import Guaka
+import TOMLDecoder
 
 let flags = [
     Flag(
@@ -29,7 +31,17 @@ var mainCommand = Guaka.Command(usage: "drstring")
 mainCommand.add(subCommand: checkCommand)
 
 if CommandLine.argc <= 1 {
-    print(mainCommand.helpMessage)
+    guard let configText = try? String(contentsOfFile: ".drstring.toml") else {
+        fputs("Couldn't read configruation from .drstring.toml\n", stderr)
+        exit(1)
+    }
+
+    guard let config = try? TOMLDecoder().decode(DrString.Configuration.self, from: configText) else {
+        fputs("Couldn't decode valid configuration from .drstring.toml", stderr)
+        exit(1)
+    }
+
+    DrString.checkCommand.run(config)
 } else {
     mainCommand.execute()
 }
