37de02bf236bad439a361f92eb300dcbc0cdebb0
Daniel Duan <daniel@duan.ca>
2019-09-21 00:03:47 -0700

    Delete old code

--- Sources/DrDecipher/DocString.swift
+++ Sources/DrDecipher/DocString.swift
@@ -1,9 +1,9 @@
 public struct DocString: Equatable {
     public struct Parameter: Equatable {
         public let name: String
-        public let description: [String]
+        public let description: [TextLeadByWhitespace]
 
-        public init(name: String, description: [String]) {
+        public init(name: String, description: [TextLeadByWhitespace]) {
             self.name = name
             self.description = description
         }
@@ -23,33 +23,3 @@ public struct DocString: Equatable {
         self.maxParameterWidth = parameters.reduce(0) { max($0, $1.name.count) }
     }
 }
-
-/*
- public struct DocString: Equatable {
-
-     public struct Parameter: Equatable {
-         public let name: String
-         /// Each line of description, including the whitespace before the content.
-         public let description: [StringLeadByWhitespace]
-
-         public init(name: String, description: [StringLeadByWhitespace]) {
-             self.name = name
-             self.description = description
-         }
-     }
-
-     public let description: [String]
-     public let parameters: [Parameter]
-     public let returns: [String]
-     public let `throws`: [String]
-     public let maxParameterWidth: Int
-
-     public init(description: [String], parameters: [Parameter], returns: [String], throws: [String]) {
-         self.description = description
-         self.parameters = parameters
-         self.returns = returns
-         self.throws = `throws`
-         self.maxParameterWidth = parameters.reduce(0) { max($0, $1.name.count) }
-     }
- }
- */
