17c2f98cd304809de28d8e8efee9002f637a6e73
Daniel Duan <daniel@duan.ca>
2019-09-25 11:11:08 -0700

    Add test for continuation lines after-column

--- Tests/DrStringTests/Fixtures/misalignedParameterDescription.swift
+++ Tests/DrStringTests/Fixtures/misalignedParameterDescription.swift
@@ -1,21 +1,25 @@
 // CHECK: Line 2 of `short`'s description is not properly vertically aligned
+// CHECK-NOT: Line 3 of `short`'s description is not properly vertically aligned
 // CHECK: Line 1 of `longer`'s description is not properly vertically aligned
 /// function description
 ///
 /// - parameter short:   short description
 /// test short description continued
+///                         test short description continued
 /// - parameter longer: longer description
 /// - parameter longest: longest description
 func f(short: Int, longer: Int, longest: Int) {
 }
 
 // CHECK: Line 2 of `short`'s description is not properly vertically aligned
+// CHECK-NOT: Line 3 of `short`'s description is not properly vertically aligned
 // CHECK: Line 1 of `longer`'s description is not properly vertically aligned
 /// function description
 ///
 /// - parameters:
 ///   - short:   short description test
 ///   short description continued
+///               test short description continued
 ///   - longer: longer description
 ///   - longest: longest description
 func g(short: Int, longer: Int, longest: Int) {
