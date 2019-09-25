73af01b49cf860ef1abd24efb5e3b7e50545e434
Daniel Duan <daniel@duan.ca>
2019-09-24 23:28:44 -0700

    Make explain lenient

--- Sources/DrString/explain.swift
+++ Sources/DrString/explain.swift
@@ -14,8 +14,7 @@ public let explainCommand = Command(
     var unrecognizedIDs = [String]()
 
     for id in arguments {
-        // TODO: make ID look-up smarter. e1, e001, 1 or 001 should match to E001, for example.
-        if let explainer = Explainer.all[id.uppercased()] {
+        if let explainer = Explainer.all[id.uppercased()] ?? guessID(id).flatMap({ Explainer.all[$0] }) {
             print(plainText(for: explainer))
         } else {
             unrecognizedIDs.append(id)
@@ -29,4 +28,25 @@ public let explainCommand = Command(
     }
 
     return nil
+}
+
+private func guessID(_ badID: String) -> String? {
+    let maybeNumber: String
+    if badID.uppercased().first == "E" {
+        maybeNumber = String(badID.dropFirst())
+    } else {
+        maybeNumber = badID
+    }
+
+    guard let n = Int(maybeNumber) else {
+        return nil
+    }
+
+    if n < 10 {
+        return "E00\(n)"
+    } else if n < 100 {
+        return "E0\(n)"
+    } else {
+        return "E\(n)"
+    }
 }
