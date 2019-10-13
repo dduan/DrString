e204e8e426cd660baabc9519a8ed8b91b8ec77f8
Daniel Duan <daniel@duan.ca>
2019-10-12 18:57:08 -0700

    [Infra] Simplify library packaging

--- Scripts/package-darwin.sh
+++ Scripts/package-darwin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUILD_PATH=.build/release
-LIB_PATH=$BUILD_PATH/lib/drstring
+LIB_PATH=$BUILD_PATH/lib
 BIN_PATH=$BUILD_PATH/bin/drstring
 SWIFT_PATH=$(xcrun --find swift)
 rm -rf $LIB_PATH
@@ -10,6 +10,6 @@ mkdir -p $LIB_PATH
 mkdir -p $BUILD_PATH/bin
 cp .build/release/drstring $BIN_PATH
 cp "$(dirname $SWIFT_PATH)/../lib/swift/macosx/lib_InternalSwiftSyntaxParser.dylib" $LIB_PATH/lib_InternalSwiftSyntaxParser.dylib
-install_name_tool -add_rpath @executable_path/../lib/drstring $BIN_PATH
+install_name_tool -add_rpath @executable_path/../lib $BIN_PATH
 tar -C $BUILD_PATH -czf drstring.tar.gz bin lib
 mv drstring.tar.gz .build
