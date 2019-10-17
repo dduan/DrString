9f631b1019b89848f49c7a8f22f2b7358732d4e9
Daniel Duan <daniel@duan.ca>
2019-10-16 22:10:06 -0700

    lowercase

--- Scripts/ubuntu.sh
+++ Scripts/ubuntu.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-command -v docker &> /dev/null  || { echo >&2 "Install docker https://www.docker.com"; exit 1; }
 if ! command -v docker > /dev/null; then
     echo "Install docker https://docker.com" >&2
     exit 1
 fi
-ACTION=$1
-SWIFT=$2
-UBUNTU=$3
-DOCKERFILE=$(mktemp)
-echo "FROM swift:$SWIFT-$UBUNTU"     > $DOCKERFILE
-echo 'ADD . DrString'                >> $DOCKERFILE
-echo "RUN cd DrString; make $ACTION" >> $DOCKERFILE
-IMAGE=drstringtesting
-docker image rm -f "$IMAGE" > /dev/null
-docker build -t "$IMAGE" -f $DOCKERFILE . && docker run --rm "$IMAGE"
+action=$1
+swift=$2
+ubuntu=$3
+dockerfile=$(mktemp)
+echo "FROM swift:$swift-$ubuntu"      > $dockerfile
+echo 'ADD . DrString'                >> $dockerfile
+echo 'WORKDIR DrString'              >> $dockerfile
+echo "RUN make $action"              >> $dockerfile
+image=drstring
+docker image rm -f "$image" > /dev/null
+docker build -t "$image" -f $dockerfile .
+docker run --rm "$image"
