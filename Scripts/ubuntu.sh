d2033a44cc5dec1dfe244d7f03b9ce4b5cd06d15
Daniel Duan <daniel@duan.ca>
2019-10-16 22:17:49 -0700

    Delete static Dockerfile

--- Scripts/ubuntu.sh
+++ Scripts/ubuntu.sh
@@ -8,10 +8,10 @@ action=$1
 swift=$2
 ubuntu=$3
 dockerfile=$(mktemp)
-echo "FROM swift:$swift-$ubuntu"      > $dockerfile
-echo 'ADD . DrString'                >> $dockerfile
-echo 'WORKDIR DrString'              >> $dockerfile
-echo "RUN make $action"              >> $dockerfile
+echo "FROM swift:$swift-$ubuntu"    >  $dockerfile
+echo 'ADD . DrString'               >> $dockerfile
+echo 'WORKDIR DrString'             >> $dockerfile
+echo "RUN make $action"             >> $dockerfile
 image=drstring
 docker image rm -f "$image" > /dev/null
 docker build -t "$image" -f $dockerfile .
