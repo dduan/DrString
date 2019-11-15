697756013fbb87f21d560d1921518e786ff482c6
Daniel Duan <daniel@duan.ca>
2019-11-14 23:46:22 -0800

    [Editor] Add tests for separator formatting
    Closes #106

---
+++ Tests/EditorTests/DocStringSeparatorTests.swift
@@ -0,0 +1,373 @@
+@testable import Decipher
+@testable import Editor
+import XCTest
+
+final class DocStringSeparatorTests: XCTestCase {
+    func testAddsMissingSeparatorAfterDescription() {
+        let doc = DocString(
+            description: [
+                .init(" ", "Description")
+            ],
+            parameterHeader: nil,
+            parameters: [
+                .init(
+                    preDashWhitespaces: " ",
+                    keyword: .init(" ", "Parameter"),
+                    name: .init(" ", "a"),
+                    preColonWhitespace: "",
+                    description: [
+                        .init(" ", "a line 1")
+                    ])
+            ],
+            returns:nil,
+            throws: nil)
+
+        let result = doc.reformat(
+            initialColumn: 0,
+            columnLimit: 100,
+            verticalAlign: false,
+            alignAfterColon: [],
+            firstLetterUpperCase: true,
+            parameterStyle: .whatever,
+            separations: [.description]
+        )
+
+        let expectation = [
+            "/// Description",
+            "///",
+            "/// - Parameter a: a line 1",
+        ]
+
+        XCTAssertEqual(result, expectation)
+    }
+
+    func testDoesNotAddSeparatorAfterDescriptionIfNotNecessary() {
+        let doc = DocString(
+            description: [
+                .init(" ", "Description"),
+                .init("", ""),
+            ],
+            parameterHeader: nil,
+            parameters: [
+                .init(
+                    preDashWhitespaces: " ",
+                    keyword: .init(" ", "Parameter"),
+                    name: .init(" ", "a"),
+                    preColonWhitespace: "",
+                    description: [
+                        .init(" ", "a line 1")
+                    ])
+            ],
+            returns:nil,
+            throws: nil)
+
+        let result = doc.reformat(
+            initialColumn: 0,
+            columnLimit: 100,
+            verticalAlign: false,
+            alignAfterColon: [],
+            firstLetterUpperCase: true,
+            parameterStyle: .whatever,
+            separations: [.description]
+        )
+
+        let expectation = [
+            "/// Description",
+            "///",
+            "/// - Parameter a: a line 1",
+        ]
+
+        XCTAssertEqual(result, expectation)
+    }
+
+    func testDoesNotAddSeparatorAfterDescriptionIfNotRequired() {
+        let doc = DocString(
+            description: [
+                .init(" ", "Description"),
+            ],
+            parameterHeader: nil,
+            parameters: [
+                .init(
+                    preDashWhitespaces: " ",
+                    keyword: .init(" ", "Parameter"),
+                    name: .init(" ", "a"),
+                    preColonWhitespace: "",
+                    description: [
+                        .init(" ", "a line 1")
+                    ])
+            ],
+            returns:nil,
+            throws: nil)
+
+        let result = doc.reformat(
+            initialColumn: 0,
+            columnLimit: 100,
+            verticalAlign: false,
+            alignAfterColon: [],
+            firstLetterUpperCase: true,
+            parameterStyle: .whatever,
+            separations: []
+        )
+
+        let expectation = [
+            "/// Description",
+            "/// - Parameter a: a line 1",
+        ]
+
+        XCTAssertEqual(result, expectation)
+    }
+
+    func testAddsMissingSeparatorAfterParameters() {
+        let doc = DocString(
+            description: [],
+            parameterHeader: nil,
+            parameters: [
+                .init(
+                    preDashWhitespaces: " ",
+                    keyword: .init(" ", "Parameter"),
+                    name: .init(" ", "a"),
+                    preColonWhitespace: "",
+                    description: [
+                        .init(" ", "a line 1")
+                    ])
+            ],
+            returns: .init(
+                preDashWhitespaces: " ",
+                keyword: .init(" ", "Returns"),
+                name: .init(" ", ""),
+                preColonWhitespace: "",
+                description: [
+                    .init(" ", "stuff")
+                ]),
+            throws: nil)
+
+        let result = doc.reformat(
+            initialColumn: 0,
+            columnLimit: 100,
+            verticalAlign: false,
+            alignAfterColon: [],
+            firstLetterUpperCase: true,
+            parameterStyle: .whatever,
+            separations: [.parameters]
+        )
+
+        let expectation = [
+            "/// - Parameter a: a line 1",
+            "///",
+            "/// - Returns: stuff"
+        ]
+
+        XCTAssertEqual(result, expectation)
+    }
+
+    func testDoesNotAddSeparatorAfterParametersIfNotNecessary() {
+        let doc = DocString(
+            description: [],
+            parameterHeader: nil,
+            parameters: [
+                .init(
+                    preDashWhitespaces: " ",
+                    keyword: .init(" ", "Parameter"),
+                    name: .init(" ", "a"),
+                    preColonWhitespace: "",
+                    description: [
+                        .init(" ", "a line 1"),
+                        .init("", ""),
+                    ])
+            ],
+            returns: .init(
+                preDashWhitespaces: " ",
+                keyword: .init(" ", "Returns"),
+                name: .init(" ", ""),
+                preColonWhitespace: "",
+                description: [
+                    .init(" ", "stuff")
+                ]),
+            throws: nil)
+
+        let result = doc.reformat(
+            initialColumn: 0,
+            columnLimit: 100,
+            verticalAlign: false,
+            alignAfterColon: [],
+            firstLetterUpperCase: true,
+            parameterStyle: .whatever,
+            separations: [.parameters]
+        )
+
+        let expectation = [
+            "/// - Parameter a: a line 1",
+            "///",
+            "/// - Returns: stuff",
+        ]
+
+        XCTAssertEqual(result, expectation)
+    }
+
+    func testDoesNotAddSeparatorAfterParametersIfNotRequired() {
+        let doc = DocString(
+            description: [],
+            parameterHeader: nil,
+            parameters: [
+                .init(
+                    preDashWhitespaces: " ",
+                    keyword: .init(" ", "Parameter"),
+                    name: .init(" ", "a"),
+                    preColonWhitespace: "",
+                    description: [
+                        .init(" ", "a line 1"),
+                    ])
+            ],
+            returns: .init(
+                preDashWhitespaces: " ",
+                keyword: .init(" ", "Returns"),
+                name: .init(" ", ""),
+                preColonWhitespace: "",
+                description: [
+                    .init(" ", "stuff")
+                ]),
+            throws: nil)
+
+        let result = doc.reformat(
+            initialColumn: 0,
+            columnLimit: 100,
+            verticalAlign: false,
+            alignAfterColon: [],
+            firstLetterUpperCase: true,
+            parameterStyle: .whatever,
+            separations: []
+        )
+
+        let expectation = [
+            "/// - Parameter a: a line 1",
+            "/// - Returns: stuff",
+        ]
+
+        XCTAssertEqual(result, expectation)
+    }
+    func testAddsMissingSeparatorAfterThrows() {
+        let doc = DocString(
+            description: [],
+            parameterHeader: nil,
+            parameters: [],
+            returns: .init(
+                preDashWhitespaces: " ",
+                keyword: .init(" ", "Returns"),
+                name: .init(" ", ""),
+                preColonWhitespace: "",
+                description: [
+                    .init(" ", "stuff")
+                ]),
+            throws: .init(
+                preDashWhitespaces: " ",
+                keyword: .init(" ", "Throws"),
+                name: .init(" ", ""),
+                preColonWhitespace: "",
+                description: [
+                    .init(" ", "stuff")
+                ])
+            )
+
+        let result = doc.reformat(
+            initialColumn: 0,
+            columnLimit: 100,
+            verticalAlign: false,
+            alignAfterColon: [],
+            firstLetterUpperCase: true,
+            parameterStyle: .whatever,
+            separations: [.throws]
+        )
+
+        let expectation = [
+            "/// - Throws: stuff",
+            "///",
+            "/// - Returns: stuff"
+        ]
+
+        XCTAssertEqual(result, expectation)
+    }
+
+    func testDoesNotAddSeparatorAfterThrowsIfNotNecessary() {
+        let doc = DocString(
+            description: [],
+            parameterHeader: nil,
+            parameters: [],
+            returns: .init(
+                preDashWhitespaces: " ",
+                keyword: .init(" ", "Returns"),
+                name: .init(" ", ""),
+                preColonWhitespace: "",
+                description: [
+                    .init(" ", "stuff"),
+                ]),
+            throws: .init(
+                preDashWhitespaces: " ",
+                keyword: .init(" ", "Throws"),
+                name: .init(" ", ""),
+                preColonWhitespace: "",
+                description: [
+                    .init(" ", "stuff"),
+                    .init("", ""),
+                ])
+            )
+
+        let result = doc.reformat(
+            initialColumn: 0,
+            columnLimit: 100,
+            verticalAlign: false,
+            alignAfterColon: [],
+            firstLetterUpperCase: true,
+            parameterStyle: .whatever,
+            separations: [.throws]
+        )
+
+        let expectation = [
+            "/// - Throws: stuff",
+            "///",
+            "/// - Returns: stuff"
+        ]
+
+        XCTAssertEqual(result, expectation)
+    }
+
+    func testDoesNotAddSeparatorAfterThrowsIfNotRequired() {
+        let doc = DocString(
+            description: [],
+            parameterHeader: nil,
+            parameters: [],
+            returns: .init(
+                preDashWhitespaces: " ",
+                keyword: .init(" ", "Returns"),
+                name: .init(" ", ""),
+                preColonWhitespace: "",
+                description: [
+                    .init(" ", "stuff"),
+                ]),
+            throws: .init(
+                preDashWhitespaces: " ",
+                keyword: .init(" ", "Throws"),
+                name: .init(" ", ""),
+                preColonWhitespace: "",
+                description: [
+                    .init(" ", "stuff"),
+                ])
+            )
+
+        let result = doc.reformat(
+            initialColumn: 0,
+            columnLimit: 100,
+            verticalAlign: false,
+            alignAfterColon: [],
+            firstLetterUpperCase: true,
+            parameterStyle: .whatever,
+            separations: []
+        )
+
+        let expectation = [
+            "/// - Throws: stuff",
+            "/// - Returns: stuff"
+        ]
+
+        XCTAssertEqual(result, expectation)
+    }
+}
