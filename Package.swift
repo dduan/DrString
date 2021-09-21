5558688f35744f0d9680292e15bedcc0d41b83fb
Keith Smiley <keithbsmiley@gmail.com>
2021-09-21 16:39:30 -0700

    Upgrade to Swift 5.5 (#252)

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(
             name: "SwiftSyntax",
             url: "https://github.com/apple/swift-syntax.git",
-            .exact("0.50400.0")
+            .exact("0.50500.0")
         ),
         .package(
             url: "https://github.com/dduan/IsTTY.git",
