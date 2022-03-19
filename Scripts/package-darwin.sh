36e3fe9270281a88bb218799211bb36f9a01cc1f
Daniel Duan <daniel@duan.ca>
2022-03-19 13:22:08 -0700

    fixup! fixup! Add completion scripts to Linux package

--- Scripts/package-darwin.sh
+++ Scripts/package-darwin.sh
@@ -6,6 +6,7 @@ BUILD_PATH=.build/apple/Products/Release
 LIB_PATH=$BUILD_PATH/lib
 BIN_PATH=$BUILD_PATH/bin
 COMPLETIONS_PATH=$BUILD_PATH/completions
+ARCHIVE=drstring_darwin.tar.gz
 rm -rf $LIB_PATH
 rm -rf $BIN_PATH
 rm -rf $COMPLETIONS_PATH
@@ -15,5 +16,5 @@ mkdir -p $COMPLETIONS_PATH
 cp -r Scripts/completions/* $COMPLETIONS_PATH
 cp $BUILD_PATH/drstring-cli $BIN_PATH/drstring
 cp $BUILD_PATH/lib_InternalSwiftSyntaxParser.dylib $LIB_PATH/lib_InternalSwiftSyntaxParser.dylib
-tar -C $BUILD_PATH -czf drstring.tar.gz bin lib completions
-mv drstring_darwin.tar.gz .build
+tar -C $BUILD_PATH -czf $ARCHIVE bin lib completions
+mv $ARCHIVE .build
--- Scripts/package-linux.sh
+++ Scripts/package-linux.sh
@@ -5,11 +5,12 @@ set -euo pipefail
 BUILD_PATH=.build/release
 BIN_PATH=$BUILD_PATH/bin
 COMPLETIONS_PATH=$BUILD_PATH/completions
+ARCHIVE=drstring_linux.tar.gz
 rm -rf $BIN_PATH
 rm -rf $COMPLETIONS_PATH
 mkdir $BIN_PATH
 mkdir $COMPLETIONS_PATH
 cp -r Scripts/completions/* $COMPLETIONS_PATH
 mv $BUILD_PATH/drstring-cli $BIN_PATH/drstring
-tar -C $BUILD_PATH -czf drstring.tar.gz bin completions
-mv drstring_linux.tar.gz .build
+tar -C $BUILD_PATH -czf $ARCHIVE bin completions
+mv $ARCHIVE .build
