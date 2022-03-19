023c2ec0052fd7121c5b7ccd8f4b68e947d3505e
Daniel Duan <daniel@duan.ca>
2022-03-19 14:04:00 -0700

    fixup! fixup! fixup! fixup! fixup! Add completion scripts to Linux package

--- Scripts/dockerarchive.sh
+++ Scripts/dockerarchive.sh
@@ -5,5 +5,5 @@ image=drstring
 archive=.build/drstring_ubuntu.tar.gz
 docker run --rm -iv${PWD}:/host-volume $image:$image sh -s <<EOF
 chown -v $(id -u):$(id -g) $archive
-cp -va $archive /host-volume
+cp -va $archive /host-volume/$archive
 EOF
