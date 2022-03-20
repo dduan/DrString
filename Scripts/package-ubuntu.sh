d3ba7b88b7d3e25acd8791f5a153314824d5bbe6
Daniel Duan <daniel@duan.ca>
2022-03-19 19:44:21 -0700

    Make Ubuntu bulid work (#272)
    * Make Ubuntu bulid work
    
    Use -static-stdlib. Add RPATH for runtime libraries. Package runtime
    libraries.
    
    * fixup! Make Ubuntu bulid work

--- Scripts/package-ubuntu.sh
+++ Scripts/package-ubuntu.sh
@@ -4,13 +4,20 @@ set -euo pipefail
 
 BUILD_PATH=.build/release
 BIN_PATH=$BUILD_PATH/bin
+LIB_PATH=$BUILD_PATH/lib
 COMPLETIONS_PATH=$BUILD_PATH/completions
 ARCHIVE=drstring_ubuntu.tar.gz
+RUNTIME_PATH=$(./Scripts/locateswift.sh)/linux
 rm -rf $BIN_PATH
 rm -rf $COMPLETIONS_PATH
+rm -rf $LIB_PATH
 mkdir $BIN_PATH
 mkdir $COMPLETIONS_PATH
+mkdir $LIB_PATH
 cp -r Scripts/completions/* $COMPLETIONS_PATH
 mv $BUILD_PATH/drstring-cli $BIN_PATH/drstring
-tar -C $BUILD_PATH -czf $ARCHIVE bin completions
+for lib in libBlocksRuntime.so lib_InternalSwiftSyntaxParser.so; do
+    cp "$RUNTIME_PATH/$lib" $LIB_PATH
+done
+tar -C $BUILD_PATH -czf $ARCHIVE bin lib completions
 mv $ARCHIVE .build
