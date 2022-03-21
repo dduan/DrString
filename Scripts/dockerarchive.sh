b5a27be9d140ad93edf545dd03447a1c6b953624
Daniel Duan <daniel@duan.ca>
2022-03-20 22:26:34 -0700

    Use Dockerfile for Ubuntu stuff

--- Scripts/dockerarchive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./Scripts/ubuntu.sh $@
-image=drstring
-archive=.build/drstring_ubuntu.tar.gz
-docker run --rm -iv${PWD}:/host-volume $image:$image sh -s <<EOF
-chown -v $(id -u):$(id -g) $archive
-cp -va $archive /host-volume
-EOF
---
+++ Scripts/ubuntuarchive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+SWIFT_VERSION=$1
+UBUNTU_RELEASE=$2
+DOCKER_ARCH=$3
+IMAGE_TAG="${SWIFT_VERSION}-${UBUNTU_RELEASE}-${DOCKER_ARCH}"
+ARCHIVE=drstring-ubuntu-${UBUNTU_RELEASE}.tar.gz
+
+docker build --platform linux/$DOCKER_ARCH --force-rm -f "Scripts/Dockerfile-${SWIFT_VERSION}-${UBUNTU_RELEASE}" --tag $IMAGE_TAG .
+
+TEMP=$(mktemp -d)
+EXES=(/usr/lib/libBlocksRuntime.so /usr/lib/lib_InternalSwiftSyntaxParser.so /usr/bin/drstring)
+ALL=( "${EXES[@]}" /etc/bash_completion.d/drstring /usr/share/zsh/vendor-completions/_drstring /usr/share/fish/completions/drstring.fish )
+for content in ${ALL[@]}; do
+    mkdir -p "$TEMP/$(dirname $content)"
+    docker run $IMAGE_TAG cat $content > "$TEMP/$content"
+done
+
+for exe in ${EXES[@]}; do
+    chmod +x "$TEMP/$exe"
+done
+
+tar -C $TEMP -czf $ARCHIVE etc usr
