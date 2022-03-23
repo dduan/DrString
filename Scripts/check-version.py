85fc340a9491b78d9a27637c4feddbbb1bd3769b
Daniel Duan <daniel@duan.ca>
2022-03-23 14:53:44 -0700

    Add script for checking versions

---
+++ Scripts/check-version.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+expected = re.search(r'let version = "(.+)"', open('Sources/DrStringCore/version.swift').read(), re.M).group(1)
+versions = {}
+versions['CHANGELOG.md'] = re.search(r'# main\s+##\s*(.+)', open('CHANGELOG.md').read(), re.M).group(1)
+
+for file in versions:
+    if expected != versions[file]:
+        print(f"version mismatch: expected {expected}; found {versions[file]} in {file}", file=sys.stderr)
+        exit(1)
